35d02bb205debafa16597e48799a275f7b96f448
EADRYC <eadrycerousmane@gmail.com>
2026-01-22 15:50:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,22 +2,22 @@ const CACHE_NAME = "eonline-cache-v1";
 const urlsToCache = [
   "/",
   "https://eadryc.github.io/eonline-pwa/manifest.json",
-  // weka images na CSS muhimu hapa
+  "https://i.imgur.com/Q1S35fF.png",  // 192px icon
+  "https://i.imgur.com/2HWmn7K.png"   // 512px icon
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
+
