026ce8909b63213d9a858df7d4e25e965427481b
EADRYC <eadrycerousmane@gmail.com>
2026-01-26 21:35:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,41 @@
-const CACHE_NAME = "eonline-cache-v1";
+/* SERVICE WORKER - E-ONLINE BUSINESS EDITION */
+const CACHE_NAME = "eonline-cache-v2"; // Badilisha v1 iwe v2 kila ukifanya update
 const urlsToCache = [
   "/",
-  "https://eadryc.github.io/eonline-pwa/manifest.json",
-  "https://i.imgur.com/Q1S35fF.png",
-  "https://i.imgur.com/2HWmn7K.png"
+  "https://www.e-online.co.tz/?utm_source=pwa",
+  "https://eadryc.github.io/eonline-pwa/manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
   self.skipWaiting();
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      // Hapa unaweza kuweka page ya Offline kama ukitengeneza
+      return caches.match("/");
+    })
   );
 });
